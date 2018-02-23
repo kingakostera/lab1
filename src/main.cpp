885a2054ecc91741e9031372be9f7b3510af17b9
kingakostera <kinga.kostera@gmail.com>
2018-02-23 10:40:24 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-int main() {return 0;}
+#include "trygonometria.h"
+
+
+int main() {
+
+	double wynik_sin=degreemath::sin_deg(90.0);
+	double wynik_cos=degreemath::cos_deg(0.0);
+	double wynik_tg=degreemath::tg_deg(45.0);
+	double wynik_ctg=degreemath::ctg_deg(45.0);
+
+	return 0;
+}
